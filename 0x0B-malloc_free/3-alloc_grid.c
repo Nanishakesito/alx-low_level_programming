62cac5706f709049506301104a8da0bfe790a8d5
Nanishakesito <narossharifo@gmail.com>
2022-06-29 03:23:37 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,52 +1,38 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: int
+ * @height: int
+ * Return: 2d array
  */
 
-
-int _strlen(char *s)
+int **alloc_grid(int width, int height)
 {
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
+int **tab, i, j;
+tab = malloc(sizeof(*tab) * height);
+
+if (width <= 0 || height <= 0 || tab == 0)
+	return (NULL);
+
+else
+{
+	for (i = 0; i < height; i++)
+	{
+		tab[i] = malloc(sizeof(**tab) * width);
+		if (tab[i] == 0)
+		{
+			/*Free everything if malloc fails*/
+			while (i--)
+				free(tab[i]);
+			free(tab);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			tab[i][j] = 0;
+	}
 }
 
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
-char *str_concat(char *s1, char *s2)
-{
-int size1, size2, i;
-char *m;
-
-if (s1 == NULL)
-	s1 = "\0";
-if (s2 == NULL)
-	s2 = "\0";
-
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-	return (0);
-
-for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		m[i] = s1[i];
-	else
-		m[i] = s2[i - size1];
-}
-m[i] = '\0';
-return (m);
+return (tab);
 }
