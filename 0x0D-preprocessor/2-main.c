708a6bc0d7f4b6cc536af5383184212f950c6fe9
nanishakesito <narossharifo@gmail.com>
2022-07-05 15:27:29 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes to standard output
+ * @c: The character to print
+ * Return: on success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * main - prints source file name
